2a050995b4f2c3ee95c746570399b41d23a92080
Ezra Apple <ezraapple.engineer@gmail.com>
2023-11-29 11:48:12 -0800

    redid lora and adjusted finetuning

--- project/lora_finetuning.py
+++ project/lora_finetuning.py
@@ -1,12 +1,13 @@
 import clip
-import torch
-import loralib as lora
+from peft import LoraConfig
 from lora_model import LoraCaptionModel, num, num_train
 from dataset import InstagramDataset
 from base_finetune import fine_tune
 from transformers import GPT2Tokenizer
 import matplotlib.pyplot as plt
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+import torch
+
+device = "cpu" # torch.device("cuda" if torch.cuda.is_available() else "cpu")
 clip_model, preprocess = clip.load("ViT-B/32", device=device, jit=False)
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 
@@ -15,12 +16,22 @@ validation_data = InstagramDataset(clip_model, preprocess, tokenizer, split="tes
 
 lora_ranks = [4, 16, 64, 256]
 for rank in lora_ranks:
-    model = LoraCaptionModel(rank)
-    lora.mark_only_lora_as_trainable(model)
-    train_loss, val_loss = fine_tune(model, train_data, validation=validation_data, epochs=1, batch_size=40, device=device)
+    config = LoraConfig(
+        r=rank,
+        lora_alpha=16,
+        target_modules=["wte", "wpe", "c_attn", "c_proj", "c_fc"],
+        lora_dropout=0.1,
+        bias="none",
+        modules_to_save=["classifier"],
+    )
+    model = LoraCaptionModel(config)
+    print(f"Training Rank {rank} LoRA | Total Parameters: {num(model)} | Trainable Parameters: {num_train(model)}")
+    train_loss, val_loss = fine_tune(model, train_data, validation=validation_data, epochs=1, batch_size=24, device=device, num_data_pts=50)
     torch.save(model.state_dict(), f"lora_{rank}.pt")
+
     plt.plot(range(len(train_loss)), train_loss)
     plt.plot(range(len(train_loss)), val_loss)
     plt.savefig(f"lora_{rank}.png")
+    plt.show()
 
 
--- project/lora_model.py
+++ project/lora_model.py
@@ -6,9 +6,10 @@ TODO
 import numpy as np
 import torch
 from torch import nn
-import loralib as lora
-from clip_model import CaptionModel
+from peft import LoraConfig, get_peft_model
+from transformers import AutoModelForImageClassification
 
+from clip_model import CaptionModel
 
 def num_train(model):
     model_parameters = filter(lambda p: p.requires_grad, model.parameters())
@@ -22,33 +23,20 @@ def num(model):
 
 
 class LoraCaptionModel(nn.Module):
-    def __init__(self, rank
-                 ):
+    def __init__(self, config):
         super().__init__()
         self.model = CaptionModel(10)
-        self.model.load_state_dict(torch.load("state_dicts/coco_weights.pt", map_location=torch.device('cpu')))
-        self.rank = rank
+        self.model.load_state_dict(self.prep_dict())
+        get_peft_model(self.model, config)
 
-        # add lora to embeddings
-        w = self.model.gpt.transformer.wte.weight
-        self.model.gpt.transformer.wte = lora.Embedding(50257, 768, r=rank)
-        self.model.gpt.transformer.wte.weight = w
-
-        # add lora to each gpt_block <-- conv1d keeps erroring
-        # for block in self.model.gpt.transformer.h:
-        #     w = block.attn.c_attn.weight
-        #     block.attn.c_attn = lora.MergedLinear(768, 2304, fan_in_fan_out=True, enable_lora=[True, False, True], r=rank)
-        #     block.attn.c_attn.weight = w
-
-        # add lora to projection map
-        map = self.model.clip_project.model
-        w = map[0].weight
-        map[0] = lora.Linear(map[0].in_features, map[0].out_features, r=rank)
-        map[0].weight = w
-
-        w = map[2].weight
-        map[2] = lora.Linear(map[2].in_features, map[2].out_features, r=rank)
-        map[2].weight = w
 
     def forward(self, tokens, prefix, mask):
         return self.model(tokens, prefix, mask)
+
+    def prep_dict(self):
+        coco_dict = torch.load("state_dicts/coco_weights.pt", map_location="cpu")
+
+        for key in coco_dict.copy().keys():
+            if key.endswith(".attn.bias") or key.endswith(".attn.masked_bias"):
+                del coco_dict[key]
+        return coco_dict
